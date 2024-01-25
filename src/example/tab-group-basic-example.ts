2a8e16371c54757fb84b5a1681419af1ab1e7ac9
JegorJakovlev <92288708+JegorJakovlev@users.noreply.github.com>
2024-01-25 16:15:28 +0200

    AngularTab

---
+++ src/example/tab-group-basic-example.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
+
+/**
+ * @title 
+ */
+@Component({
+  selector: 'tab-group-basic-example',
+  templateUrl: 'tab-group-basic-example.html',
+  standalone: true,
+  imports: [MatTabsModule],
+})
+export class TabGroupBasicExample {}
+
+
